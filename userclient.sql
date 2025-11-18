5b17a56ce3f6d6f532607ed42c71907675ef2c72
codes <atifimranch2002@gmail.com>
2025-11-18 17:58:48 +0500

    Add files via upload

---
+++ userclient.sql
@@ -0,0 +1,25 @@
+create database Information;
+use Information;
+create table userInfo(
+IdNo int primary key auto_increment,
+Name varchar(20),
+phoneNumber varchar(15),
+address varchar(100),		
+email varchar(35)
+);	
+ALTER TABLE userInfo AUTO_INCREMENT = 1;
+select * from userInfo;
+ALTER TABLE userInfo MODIFY IdNo INT NOT NULL AUTO_INCREMENT;
+SHOW CREATE TABLE userInfo;
+ALTER TABLE userinfo 
+MODIFY IdNo INT NOT NULL,
+ADD PRIMARY KEY (IdNo);
+ALTER TABLE userinfo 
+MODIFY IdNo INT NOT NULL AUTO_INCREMENT;
+ALTER TABLE userinfo 
+MODIFY IdNo INT NOT NULL AUTO_INCREMENT;
+SET SQL_SAFE_UPDATES = 0;
+delete from userInfo ;
+drop table userinfo;
+
+
